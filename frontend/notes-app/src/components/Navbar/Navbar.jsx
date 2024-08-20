549ee7b0bf54eadd9c25fc830f44fbf43b5c4dc8
trung <leqtrungggg3105@gmail.com>
2024-08-20 10:28:19 +0700

    push Login screen UI & Password input component

---
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+        <h2 className={styles.title}>Notes</h2>
+    </div>
+  );
+}
+
+export default Navbar;
--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
+import styles from './Login.module.css'
+import PasswordInput from '../../components/Input/PasswordInput'
+import { validateEmail } from '../../utils/helper'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("")
+
+    //Login API Call
+  };
+
   return (
-    <div>Login</div>
+    <>
+      <Navbar />
+
+      <div className={styles.container}>
+        <div className={styles.formWrapper}>
+          <form onSubmit={handleLogin}>
+            <h4 className={styles.title}>Login</h4>
+            
+            <input 
+              type="text" 
+              placeholder="Email" 
+              className='input-box' 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} 
+            /> 
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type="submit" className="btn-primary">Login</button>
+
+            <p className={styles.textSmall}>
+              Not registered yet?    {""}
+              <Link to="/signUp" className="font-medium text-primary underline">Create an Account</Link> 
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
