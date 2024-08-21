d07e3748e3507d1cb7c766f9cdc1022c86362233
trung <leqtrungggg3105@gmail.com>
2024-08-21 11:01:51 +0700

    push Navbar Profile Card & Search Bar Component

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import styles from './Navbar.module.css';
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+
+  const handleSearch = () => {};
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
   return (
     <div className={styles.navbar}>
-        <h2 className={styles.title}>Notes</h2>
+      <h2 className={styles.title}>Notes</h2>
+
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
-}
+};
 
 export default Navbar;
